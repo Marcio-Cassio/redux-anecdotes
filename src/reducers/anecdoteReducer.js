28431a923e85e95d0921cfa25ba83d51c0c76185
Marcio <marcioscontact@gmail.com>
2026-03-12 19:48:53 -0300

    Finished exercises 6.2-6.8 Part 6

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -19,11 +19,39 @@ const asObject = anecdote => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+const anecdoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
 
-  return state
+      return state.map(anecdote =>
+        anecdote.id !== id
+          ? anecdote
+          : { ...anecdote, votes: anecdote.votes + 1 }
+      )
+    }
+
+    case 'NEW_ANECDOTE': {
+      return state.concat(action.payload)
+    }
+
+    default:
+      return state
+  }
 }
 
-export default reducer
+export const voteAnecdote = id => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
+}
+
+export const createAnecdote = content => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: asObject(content)
+  }
+}
+
+export default anecdoteReducer
